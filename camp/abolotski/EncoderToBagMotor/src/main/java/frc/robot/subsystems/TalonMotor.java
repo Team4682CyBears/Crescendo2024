7531fe4ae74803efb8672502cdaa39aed92c13db
asherbolotski <70773941+asherbolotski@users.noreply.github.com>
2023-09-13 15:13:41 -0700

    somestuff
    somestuff

--- camp/abolotski/EncoderToBagMotor/src/main/java/frc/robot/subsystems/TalonMotor.java
+++ camp/abolotski/EncoderToBagMotor/src/main/java/frc/robot/subsystems/TalonMotor.java
@@ -6,11 +6,22 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import frc.robot.Constants;
 
-public class BagMotor extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  public BagMotor() {
-  }
+public class TalonMotor extends SubsystemBase{
+  private final WPI_TalonFX mainMotor = new WPI_TalonFX(Constants.JoystickMotorCanId);
+
+
+public EpicTalonMotor() {
+  mainMotor.configFactoryDefault();
+}
 
   /**
    * Example command factory method.
