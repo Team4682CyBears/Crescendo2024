b8ec02df6246d16df95da42187d78a92660b36b1
Mike McAdams <56608471+mmcadamsms@users.noreply.github.com>
2023-10-27 13:19:34 -0700

    Update of angular acceleration based on ask from John in text

--- minibear/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ minibear/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -82,7 +82,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
   public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = MAX_VELOCITY_METERS_PER_SECOND /
           Math.hypot(DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DRIVETRAIN_WHEELBASE_METERS / 2.0);
   public static final double MIN_ANGULAR_VELOCITY_BOUNDARY_RADIANS_PER_SECOND = MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND * 0.06; // 0.06 a magic number based on testing
-  private double MAX_ANGULAR_ACCELERATION_RADIANS_PER_SECOND_SQUARED = 10.0;
+  private double MAX_ANGULAR_ACCELERATION_RADIANS_PER_SECOND_SQUARED = 1.0;
 
   private static final int PositionHistoryWindowTimeMilliseconds = 5000;
   private static final int CommandSchedulerPeriodMilliseconds = 20;
