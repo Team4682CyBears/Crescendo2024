b655ebdf31f56ce2701e57a53176a0df660e5b55
mcadamswa <mcadamswa@hotmail.com>
2023-10-28 08:39:44 -0700

    updated pdh can id

--- minibear/src/main/java/frc/robot/Constants.java
+++ minibear/src/main/java/frc/robot/Constants.java
@@ -112,7 +112,7 @@ public final class Constants {
 
     // ********************************************************************
     // PowerDistributionPanel Constants
-    public static final int currentPowerDistributionPanelCanId = 19;
+    public static final int currentPowerDistributionPanelCanId = 22;
     public static final ModuleType currentPowerDistributionPanelType = ModuleType.kRev;
     public static final double overcurrentRumbleTimeSeconds = 0.25;
     public static final int EveryBotMotorPdpPortId = 8;
