68dc9ace0e1755229f1195e665d58f96b07cd4c5
mcadamswa <mcadamswa@hotmail.com>
2024-01-10 14:53:37 -0800

    zerod sweve

--- minibear/src/main/java/frc/robot/Constants.java
+++ minibear/src/main/java/frc/robot/Constants.java
@@ -31,27 +31,27 @@ public final class Constants {
      *
      * Should be measured from center to center.
      */
-    public static final double DRIVETRAIN_WHEELBASE_METERS = Units.inchesToMeters(17.179);
+    public static final double DRIVETRAIN_WHEELBASE_METERS = Units.inchesToMeters(0.0);
 
     public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1;
     public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 2;
     public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 3;
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(335.21);
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(78.5);
 
     public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 4;
     public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 5;
     public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 6;
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(29.09 + 180);
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(30.14);
 
     public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 7;
     public static final int BACK_LEFT_MODULE_STEER_MOTOR = 8;
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 9;
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(236.25+5.09 + 296.8);
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(334.6-180.0);
 
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 10;
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 11;
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 12;
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(322.03);
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(148.3-180.0);
 
     // *****************************************************************
     // standard stuff constants - motors rotation, etc.
