51edab9a84bfd60f8dc5325cb69dc5ff27f43d07
mcadamswa <mcadamswa@hotmail.com>
2024-01-10 16:17:08 -0800

    updated for different module

--- minibear/src/main/java/frc/robot/swerveHelpers/WcpModuleConfigurations.java
+++ minibear/src/main/java/frc/robot/swerveHelpers/WcpModuleConfigurations.java
@@ -14,10 +14,10 @@ import com.swervedrivespecialties.swervelib.ModuleConfiguration;
 
 public class WcpModuleConfigurations {
     public static final ModuleConfiguration SWERVEX = new ModuleConfiguration(
-            0.1016,  //4" in meters
-            1/6.54, // 7.85:1 (10:34 -> 26:20 -> 15:45)
-            true, //three reductions
-            1/15.43, // 15.43:1 (8:24 -> 14:72)
-            false //two reductions
+        0.1016,  //4" in meters
+        1/7.85, // 7.85:1 (10:34 -> 26:20 -> 15:45)
+        true, //three reductions
+        1/15.43, // 15.43:1 (8:24 -> 14:72)
+        false //two reductions
     );
 }
