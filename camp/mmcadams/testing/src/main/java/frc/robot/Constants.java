d5529d54f3442922c519a8f3e3993ccba1116fe3
Mike McAdams <mcadamswa@hotmail.com>
2024-02-06 14:51:28 -0800

    mike changed a constant and didnt commit

--- camp/mmcadams/testing/src/main/java/frc/robot/Constants.java
+++ camp/mmcadams/testing/src/main/java/frc/robot/Constants.java
@@ -19,7 +19,7 @@ public final class Constants {
   public static final double talonMaximumRevolutionsPerMinute = 6380;
   public static final double CtreTalonFx500EncoderTicksPerRevolution = 2048; 
 
-  public static final int leftTalonShooterMotorCanId = 10;
+  public static final int leftTalonShooterMotorCanId = 7;
   public static TalonFXInvertType leftTalonShooterMotorDefaultDirection = TalonFXInvertType.Clockwise;  
   public static final int rightTalonShooterMotorCanId = 8;
   public static TalonFXInvertType rightTalonShooterMotorDefaultDirection = TalonFXInvertType.CounterClockwise;  
