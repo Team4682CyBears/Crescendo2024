d58be6e7bd13ab3d0f78b183033c3bbb1c093082
Mike McAdams <56608471+mmcadamsms@users.noreply.github.com>
2024-02-24 18:27:36 -0800

    Create FeederMode.java

---
+++ ted/src/main/java/frc/robot/common/FeederMode.java
@@ -0,0 +1,16 @@
+// ************************************************************
+// Bishop Blanchet Robotics
+// Home of the Cybears
+// FRC - Crescendo - 2024
+// File: FeederMode.java
+// Intent: Forms enum of where notes will be fed into.
+// ************************************************************
+
+// ʕ •ᴥ•ʔ ʕ•ᴥ•  ʔ ʕ  •ᴥ•ʔ ʕ •`ᴥ´•ʔ ʕ° •° ʔ ʕ •ᴥ•ʔ ʕ•ᴥ•  ʔ ʕ  •ᴥ•ʔ ʕ •`ᴥ´•ʔ ʕ° •° ʔ 
+
+package frc.robot.common;
+
+public enum FeederMode{
+    FeedToShooter,
+    FeedToDunker
+}
