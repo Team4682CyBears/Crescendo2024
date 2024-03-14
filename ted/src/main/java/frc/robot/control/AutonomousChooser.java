48938145269983773a8d64d860e396daf0ada225
asherbolotski <70773941+asherbolotski@users.noreply.github.com>
2024-03-13 17:08:57 -0700

    Update AutonomousChooser.java

--- ted/src/main/java/frc/robot/control/AutonomousChooser.java
+++ ted/src/main/java/frc/robot/control/AutonomousChooser.java
@@ -21,15 +21,12 @@ import com.pathplanner.lib.util.ReplanningConfig;
 
 import frc.robot.commands.AutoShooterSpinUpCommand;
 import frc.robot.commands.ButtonPressCommand;
-import frc.robot.commands.FeedNoteCommand;
 import frc.robot.commands.IntakeAndFeedNoteCommand;
-import frc.robot.commands.ShooterShootCommand;
 import frc.robot.commands.RemoveNoteCommand;
 import frc.robot.commands.ShooterSetAngleCommand;
 import frc.robot.commands.ShooterSetAngleWithVisionOneShotCommand;
 import frc.robot.common.FeederMode;
 import frc.robot.commands.FeederLaunchNote;
-import frc.robot.control.SubsystemCollection;
 
 public class AutonomousChooser {
     private SubsystemCollection subsystems;
