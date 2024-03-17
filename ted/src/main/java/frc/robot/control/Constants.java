a76261027188e0d4259dca3a5b5ef13f61d54931
Mike McAdams <mcadamswa@hotmail.com>
2024-03-17 11:10:06 -0700

    .

--- ted/src/main/java/frc/robot/control/Constants.java
+++ ted/src/main/java/frc/robot/control/Constants.java
@@ -32,10 +32,10 @@ public final class Constants {
         Units.inchesToMeters(22.75), 
         WcpModuleConfigurations.TED,
         // SUBTRACT the values you find in shuffleboard
-        Math.toRadians(-215.15 - 178.76 - 180.0 -358.7), // FRONT LEFT
+        Math.toRadians(-215.15 - 178.76 - 180.0 - 358.7), // FRONT LEFT
         Math.toRadians(-180.61 - 95.27 - 358.6 -169.8 - 150.59), // FRONT RIGHT 
-        Math.toRadians(-191.33 - 257.52 -357.3  - 3.6), // BACK LEFT
-        Math.toRadians(-58.35 - 177.27 - 180.0 - 2.0)); // BACK RIGHT
+        Math.toRadians(-191.33 - 257.52 -357.3  - 3.6 - 355.67), // BACK LEFT
+        Math.toRadians(-58.35 - 177.27 - 180.0 - 2.0 - 358.50)); // BACK RIGHT
 
     //////////////////// BABYBEAR DRIVETRAIN ////////////////////
     public static final DrivetrainSwerveConfig babybearDrivetrainConfig = new DrivetrainSwerveConfig (
