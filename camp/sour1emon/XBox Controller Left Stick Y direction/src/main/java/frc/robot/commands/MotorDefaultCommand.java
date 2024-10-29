458acd40c27e3f892ad26c76c8fbbede91cc7d89
Sour1emon <isaac.bess@outlook.com>
2024-10-29 15:33:49 -0700

    Add javadoc comments

--- camp/sour1emon/XBox Controller Left Stick Y direction/src/main/java/frc/robot/commands/MotorDefaultCommand.java
+++ camp/sour1emon/XBox Controller Left Stick Y direction/src/main/java/frc/robot/commands/MotorDefaultCommand.java
@@ -4,16 +4,30 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.MotorSubsystem;
 
+/**
+ * A command that sets the speed of a motor subsystem based on the Y direction
+ * of the left stick of an Xbox controller.
+ */
 public class MotorDefaultCommand extends Command {
     private XboxController controller;
     private MotorSubsystem motorSubsystem;
 
+    /**
+     * Constructs a MotorDefaultCommand.
+     * 
+     * @param motorSubsystem the motor subsystem to control
+     * @param controller     the Xbox controller to read input from
+     */
     public MotorDefaultCommand(MotorSubsystem motorSubsystem, XboxController controller) {
         this.motorSubsystem = motorSubsystem;
         this.controller = controller;
         addRequirements(motorSubsystem);
     }
 
+    /**
+     * Executes the command by setting the speed of the motor subsystem based on the
+     * left Y direction of the controller.
+     */
     @Override
     public void execute() {
         motorSubsystem.setSpeed(controller.getLeftY());
--- camp/sour1emon/XBox Controller Left Stick Y direction/src/main/java/frc/robot/subsystems/MotorSubsystem.java
+++ camp/sour1emon/XBox Controller Left Stick Y direction/src/main/java/frc/robot/subsystems/MotorSubsystem.java
@@ -5,22 +5,48 @@ import com.revrobotics.CANSparkLowLevel.*;
 
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
+/**
+ * Represents a motor subsystem that controls a single motor.
+ */
 public class MotorSubsystem implements Subsystem {
 
     private CANSparkMax motor;
 
+    /**
+     * Constructs a MotorSubsystem object with the specified motor port.
+     * 
+     * @param motorPort the port number of the motor
+     */
     public MotorSubsystem(int motorPort) {
         this.motor = new CANSparkMax(motorPort, MotorType.kBrushless);
     }
 
+    /**
+     * Clamps a value between a minimum and maximum range.
+     *
+     * @param value The value to be clamped.
+     * @param min   The minimum value of the range.
+     * @param max   The maximum value of the range.
+     * @return The clamped value.
+     */
     private double clamp(double value, double min, double max) {
         return Math.max(min, Math.min(max, value));
     }
 
+    /**
+     * Sets the speed of the motor.
+     * 
+     * @param speed the desired speed of the motor (-1.0 to 1.0)
+     */
     public void setSpeed(double speed) {
         motor.set(clamp(speed, -1.0, 1.0));
     }
 
+    /**
+     * Gets the current speed of the motor.
+     * 
+     * @return the current speed of the motor
+     */
     public double getSpeed() {
         return motor.get();
     }
