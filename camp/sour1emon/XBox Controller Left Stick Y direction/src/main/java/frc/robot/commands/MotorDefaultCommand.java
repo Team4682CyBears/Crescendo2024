8763577435e0c9ae021aee90057925e66186440f
Sour1emon <isaac.bess@outlook.com>
2024-11-29 12:07:09 -0800

    Updated code to use correct template

--- camp/sour1emon/XBox Controller Left Stick Y direction/src/main/java/frc/robot/commands/MotorDefaultCommand.java
+++ camp/sour1emon/XBox Controller Left Stick Y direction/src/main/java/frc/robot/commands/MotorDefaultCommand.java
@@ -1,26 +1,27 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.XboxController;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.MotorSubsystem;
+import java.util.function.DoubleSupplier;
 
 /**
  * A command that sets the speed of a motor subsystem based on the Y direction
  * of the left stick of an Xbox controller.
  */
 public class MotorDefaultCommand extends Command {
-    private XboxController controller;
     private MotorSubsystem motorSubsystem;
+    private DoubleSupplier doubleSupplier;
 
     /**
      * Constructs a MotorDefaultCommand.
      * 
      * @param motorSubsystem the motor subsystem to control
-     * @param controller     the Xbox controller to read input from
+     * @param doubleSupplier double input for the motor speed
      */
-    public MotorDefaultCommand(MotorSubsystem motorSubsystem, XboxController controller) {
+    public MotorDefaultCommand(MotorSubsystem motorSubsystem, DoubleSupplier doubleSupplier) {
         this.motorSubsystem = motorSubsystem;
-        this.controller = controller;
+        this.doubleSupplier = doubleSupplier;
         addRequirements(motorSubsystem);
     }
 
@@ -30,6 +31,6 @@ public class MotorDefaultCommand extends Command {
      */
     @Override
     public void execute() {
-        motorSubsystem.setSpeed(controller.getLeftY());
+        motorSubsystem.setSpeed(doubleSupplier.getAsDouble());
     }
 }
