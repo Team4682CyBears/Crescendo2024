9a10c5c78a76d029b510a1bf0fed297c08a3f961
4682cybears <4682cybears@gmail.com>
2024-12-04 16:39:19 -0800

    debugging changes

--- camp/mgetachew/mgetachewMiniProject/src/main/java/frc/robot/commands/moveMotor.java
+++ camp/mgetachew/mgetachewMiniProject/src/main/java/frc/robot/commands/moveMotor.java
@@ -21,7 +21,6 @@ public class moveMotor extends Command{
 
     @Override
     public void execute(){
-        super.execute();
         if(leftBumperSupplier.getAsBoolean()){
             motorTalonToMove.motorReverse();
         } else if(rightBumperSupplier.getAsBoolean()){
@@ -33,6 +32,6 @@ public class moveMotor extends Command{
 
     @Override
     public boolean isFinished(){
-        return super.isFinished();
+        return false;
     }
 }
--- camp/mgetachew/mgetachewMiniProject/src/main/java/frc/robot/subsystems/MotorTalon.java
+++ camp/mgetachew/mgetachewMiniProject/src/main/java/frc/robot/subsystems/MotorTalon.java
@@ -1,10 +1,6 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.motorcontrol.PWMTalonFX;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
-
 import com.ctre.phoenix6.configs.Slot0Configs;
-import com.ctre.phoenix6.configs.Slot1Configs;
 import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VelocityVoltage;
@@ -12,43 +8,43 @@ import com.ctre.phoenix6.controls.VoltageOut;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix6.hardware.TalonFX;
 import frc.robot.Constants;
-import edu.wpi.first.wpilibj2.command.Command;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
-public class MotorTalon extends SubsystemBase{
+public class MotorTalon extends SubsystemBase {
     private TalonFX motorTalon;
+    private double desiredVelocity = 0;
+    private double previousVelocity = 0;
     private final VelocityVoltage motorTalonVelocityController = new VelocityVoltage(0);
     private final VoltageOut motorTalonVoltageController = new VoltageOut(0);
     private TalonFXConfiguration motorTalonMotorConfiguration = null;
     private Slot0Configs motorRpmGains = new Slot0Configs().withKP(0.36).withKI(0.1).withKD(0.0075).withKV(0.10);
 
-    private Slot0Configs motorTalonMotorHighRpmGains = new Slot0Configs().withKP(0.36).withKI(0.1).withKD(0.0075).withKV(0.10);
-    private Slot1Configs motorTalonMotorLowRpmGains = new Slot1Configs().withKP(0.20).withKI(0.2).withKD(0.0075).withKV(0.05);
-
     private NeutralModeValue outfeedMotorTargetNeutralModeValue = NeutralModeValue.Coast;
     private static final double kMinDeadband = 0.001;
-      
 
-    public MotorTalon(){
+    public MotorTalon() {
         motorTalon = new TalonFX(Constants.OperatorConstants.motorTalonPort);
         configureMotor();
         System.out.print("Motor doing stuff");
     }
 
-    public void motorReverse(){
-        motorTalon.setControl(this.motorTalonVelocityController.withVelocity(-1));
+    public void motorReverse() {
+        System.out.println("Reverse...");
+        this.desiredVelocity = -1;
     }
 
-    public void motorForwards(){
-        motorTalon.setControl(this.motorTalonVelocityController.withVelocity(1));
+    public void motorForwards() {
+        System.out.println("Forward...");
+        this.desiredVelocity = 1;
     }
 
-    public void motorStop(){
-        motorTalon.setControl(this.motorTalonVoltageController.withOutput(0));
+    public void motorStop() {
+        System.out.println("Stop.");
+        this.desiredVelocity = 0;
     }
 
-    private void configureMotor(){
+    private void configureMotor() {
         // Config motor
         motorTalonMotorConfiguration = new TalonFXConfiguration();
         motorTalonMotorConfiguration.MotorOutput.NeutralMode = this.outfeedMotorTargetNeutralModeValue;
@@ -65,20 +61,25 @@ public class MotorTalon extends SubsystemBase{
         motorTalonMotorConfiguration.CurrentLimits.SupplyCurrentLimitEnable = true;
         // motor direction
         motorTalonMotorConfiguration.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-    
+
         StatusCode response = motorTalon.getConfigurator().apply(this.motorTalonMotorConfiguration);
         if (!response.isOK()) {
-          System.out.println(
-              "TalonFX ID " + motorTalon.getDeviceID() + " failed config with error " + response.toString());
+            System.out.println(
+                    "TalonFX ID " + motorTalon.getDeviceID() + " failed config with error " + response.toString());
         }
-    
-        }
-    
-    
+
+    }
 
     @Override
     public void periodic() {
-        System.out.println(motorTalon.get());
-        super.periodic();
+        if (desiredVelocity != previousVelocity) {
+            // System.out.println(motorTalon.get());
+            if (desiredVelocity == 0) {
+                motorTalon.setControl(this.motorTalonVoltageController.withOutput(0));
+            } else {
+                motorTalon.setControl(this.motorTalonVelocityController.withVelocity(desiredVelocity));
+            }
+        }
+        this.previousVelocity = this.desiredVelocity;
     }
 }
